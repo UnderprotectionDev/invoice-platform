7ea8172d702603d024e7fcc1a73da7ceac31c9c2
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-11 15:24:14 +0300

    Update dependencies and enhance dashboard layout with new components
    - Added @radix-ui/react-avatar and recharts dependencies to package.json and pnpm-lock.yaml for improved UI and data visualization.
    - Refactored the dashboard layout to include DashboardBlocks, InvoiceGraph, and RecentInvoices components, enhancing the user interface and functionality.
    - Removed the sign-out form from the dashboard for a cleaner layout.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
-import { signOut } from "../utils/auth";
+import { DashboardBlocks } from "../components/dashboard-blocks";
+import { InvoiceGraph } from "../components/invoice-graph";
+import { RecentInvoices } from "../components/recent-invoices";
 import { requireUser } from "../utils/hooks";
 
 export default async function DashboardRoute() {
@@ -6,15 +8,11 @@ export default async function DashboardRoute() {
 
   return (
     <div>
-      <h1>hello from the dashboard route</h1>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
+      <DashboardBlocks />
+      <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
+        <InvoiceGraph />
+        <RecentInvoices />
+      </div>
     </div>
   );
 }
