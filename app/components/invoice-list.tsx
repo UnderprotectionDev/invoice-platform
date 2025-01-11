096091e296b2cc180c892ef2e12b54575e3eb349
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-11 18:01:57 +0300

    Dashboard Analytics

--- app/components/invoice-list.tsx
+++ app/components/invoice-list.tsx
@@ -11,6 +11,7 @@ import prisma from "../utils/db";
 import { requireUser } from "../utils/hooks";
 import { Badge } from "@/components/ui/badge";
 import { formatCurrency } from "../utils/formatCurrency";
+import { EmptyState } from "./empty-state";
 
 async function getData(userId: string) {
   const data = await prisma.invoice.findMany({
@@ -38,42 +39,53 @@ export async function InvoiceList() {
   const session = await requireUser();
   const data = await getData(session.user?.id as string);
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Invoice ID</TableHead>
-          <TableHead>Customer</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Date</TableHead>
-          <TableHead className="text-right">Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((invoice) => (
-          <TableRow key={invoice.id}>
-            <TableCell>#{invoice.invoiceNumber}</TableCell>
-            <TableCell>{invoice.clientName}</TableCell>
-            <TableCell>
-              {formatCurrency({
-                amount: invoice.total,
-                currency: invoice.currency as "USD" | "EUR",
-              })}
-            </TableCell>
-            <TableCell>
-              <Badge>{invoice.status}</Badge>
-            </TableCell>
-            <TableCell>
-              {new Intl.DateTimeFormat("en-US", {
-                dateStyle: "medium",
-              }).format(invoice.createdAt)}
-            </TableCell>
-            <TableCell className="text-right">
-              <InvoiceActions status={invoice.status} id={invoice.id} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      {data.length === 0 ? (
+        <EmptyState
+          title="No invoices found"
+          description="Create an invoice to get started"
+          buttontext="Create invoice"
+          href="/dashboard/invoices/create"
+        />
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Invoice ID</TableHead>
+              <TableHead>Customer</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((invoice) => (
+              <TableRow key={invoice.id}>
+                <TableCell>#{invoice.invoiceNumber}</TableCell>
+                <TableCell>{invoice.clientName}</TableCell>
+                <TableCell>
+                  {formatCurrency({
+                    amount: invoice.total,
+                    currency: invoice.currency as any,
+                  })}
+                </TableCell>
+                <TableCell>
+                  <Badge>{invoice.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  {new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "medium",
+                  }).format(invoice.createdAt)}
+                </TableCell>
+                <TableCell className="text-right">
+                  <InvoiceActions status={invoice.status} id={invoice.id} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,46 @@
+import { Suspense } from "react";
 import { DashboardBlocks } from "../components/dashboard-blocks";
 import { InvoiceGraph } from "../components/invoice-graph";
 import { RecentInvoices } from "../components/recent-invoices";
+import prisma from "../utils/db";
 import { requireUser } from "../utils/hooks";
+import { Skeleton } from "@/components/ui/skeleton";
+import { EmptyState } from "../components/empty-state";
+
+async function getData(userId: string) {
+  const data = await prisma.invoice.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return data;
+}
 
 export default async function DashboardRoute() {
   const session = await requireUser();
-
+  const data = await getData(session.user?.id as string);
   return (
-    <div>
-      <DashboardBlocks />
-      <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
-        <InvoiceGraph />
-        <RecentInvoices />
-      </div>
-    </div>
+    <>
+      {data.length < 1 ? (
+        <EmptyState
+          title="No invoices found"
+          description="Create an invoice to see it right here"
+          buttontext="Create Invoice"
+          href="/dashboard/invoices/create"
+        />
+      ) : (
+        <Suspense fallback={<Skeleton className="w-full h-full flex-1" />}>
+          <DashboardBlocks />
+          <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
+            <InvoiceGraph />
+            <RecentInvoices />
+          </div>
+        </Suspense>
+      )}
+    </>
   );
 }
